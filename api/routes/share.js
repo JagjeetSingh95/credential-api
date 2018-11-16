1547f9529d807f0d3abf5b1256f10c7ada5ac738
jaggisingh <jagsingh.96@gmail.com>
2018-11-16 17:53:19 +0530

    remove nodemon

--- api/routes/share.js
+++ api/routes/share.js
@@ -35,7 +35,6 @@ router.get('/', authCheck, (req, res, next) => {
             });
          })
          .catch(err => {
-            console.log(err, "err")
             res.status(500).json({
                 error: err
             })
